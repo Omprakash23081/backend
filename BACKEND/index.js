f00e03b6bb07133565dde6fde153188c24ca4d1b
omprakash kumar <omprakashkumarglb@gmail.com>
2025-05-02 22:11:52 +0530

    add log for testing purpose

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -11,15 +11,14 @@ app.get("/about", (req, res) => {
     description: "World",
   };
 
-<<<<<<< HEAD
   res.send(data);
-=======
+});
 app.get("/", (req, res) => {
   console.log("hellow duniya");
-  res.send("hellow Omprakash kumar how was your day"); // Send response to browser
->>>>>>> 618bfa2a6e57c7cc1cf49cf70b024cf37f78bded
+  res.send("hellow Omprakash kumar how was your day");
 });
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${port}`);
 });
+console.log("hellow kae h ap lodg pata nahi kase hoga ye sab kuch");
